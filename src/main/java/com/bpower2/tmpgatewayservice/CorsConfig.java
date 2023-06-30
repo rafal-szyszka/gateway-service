73133dc248f0a3d801da25c2e0a26846a3cec9bc
Rafał Szyszka <rafal.szyszka@bpower2.com>
2023-06-30 20:14:54 +0200

    CORS config

--- src/main/java/com/bpower2/tmpgatewayservice/CorsConfig.java
+++ src/main/java/com/bpower2/tmpgatewayservice/CorsConfig.java
@@ -4,10 +4,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
-public class CorsConfig implements WebMvcConfigurer {
+//@Configuration
+public class CorsConfig {
 
-    @Override
+//    @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowCredentials(true)
